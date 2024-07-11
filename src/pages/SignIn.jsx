a6d73a1f7f97a55902fd2a2ffae5994e6a0573cf
samim akter <samimakter9641@gmail.com>
2024-07-11 18:13:57 +0530

    create sign in, sign up, forgot password pages and oauth component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useState } from "react";
+import { IoMdEyeOff } from "react-icons/io";
+import { IoMdEye } from "react-icons/io";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default SignIn
+  const { email, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto ">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://images.pexels.com/photos/101808/pexels-photo-101808.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="key"
+            className="w-full rounded-2xl"
+          />
+        </div>
+
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="Email address"
+              className="w-full mb-6 px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+            />
+
+            <div className="relative mb-6">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={password}
+                onChange={handleChange}
+                placeholder="Password"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              />
+
+              {showPassword ? (
+                <IoMdEye
+                  size={24}
+                  className="absolute right-3 top-3 text-gray-600"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                />
+              ) : (
+                <IoMdEyeOff
+                  size={24}
+                  className="absolute right-3 top-3 text-gray-600"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                />
+              )}
+            </div>
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6 ">
+                Don't have an account?{" "}
+                <Link
+                  to="/sign-up"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
+                >
+                  Register
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/forgot-password"
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out ml-1"
+                >
+                  Forgot password?
+                </Link>
+              </p>
+            </div>
+            <button
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 hover:shadow-lg active:bg-blue-800"
+              type="submit"
+            >
+              Sign in
+            </button>
+            <div
+              className="items-center flex my-4 before:border-t before:flex-1  before:border-gray-300 
+          after:border-t after:flex-1  after:border-gray-300
+          "
+            >
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignIn;
